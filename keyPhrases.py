68b8bad5876766aedf4e6380c0cc322244b05eb7
catherinelee274 <catherinelee274@yahoo.com>
2019-10-26 04:46:52 -0700

    keyphrases is reduced to first 3

--- keyPhrases.py
+++ keyPhrases.py
@@ -18,23 +18,29 @@ documents = [
     {
         "id": "1",
         "language": "en",
-        "text": "We present a model that generates natural language descriptions of images and their regions. Our approach leverages datasets of images and their sentence descriptions to learn about the inter-modal correspondences between language and visual data. Our alignment model is based on a"
+        "text": "We present a model that generates natural language descriptions of images and their regions. Our approach leverages datasets of images and their sentence descriptions to learn about the inter-modal correspondences between language and visual data. Our alignment model is based on a "
                 + "novel combination of Convolutional Neural Networks over " +
-                "image regions, bidirectional Recurrent Neural Networks" +
-                 "over sentences, and a structured objective that aligns the" +
-                 "two modalities through a multimodal embedding. We then" +
-                 "describe a Multimodal Recurrent Neural Network architecture that uses the inferred alignments to learn to generate" +
-                 "novel descriptions of image regions. We demonstrate that" +
-                 "our alignment model produces state of the art results in retrieval experiments on Flickr8K, Flickr30K and MSCOCO" +
-                 "datasets. We then show that the generated descriptions significantly outperform retrieval baselines on both full images" +
+                "image regions, bidirectional Recurrent Neural Networks " +
+                 "over sentences, and a structured objective that aligns the " +
+                 "two modalities through a multimodal embedding. We then " +
+                 "describe a Multimodal Recurrent Neural Network architecture that uses the inferred alignments to learn to generate " +
+                 "novel descriptions of image regions. We demonstrate that " +
+                 "our alignment model produces state of the art results in retrieval experiments on Flickr8K, Flickr30K and MSCOCO " +
+                 "datasets. We then show that the generated descriptions significantly outperform retrieval baselines on both full images " +
                 "and on a new dataset of region-level annotations."
                 
     }
 ]
 response = text_analytics.key_phrases(documents=documents)
 
+#PRINT ONLY the first 3 (the more important phrases come earlier)
+
+#but for later compare more tags (like maybe 5)
 for document in response.documents:
     print("Document Id: ", document.id)
     print("\tKey Phrases:")
-    for phrase in document.key_phrases:
-        print("\t\t", phrase)
+    length = len(document.key_phrases)
+    i = 0
+    while i < 4 and i < length:
+        print("\t\t", document.key_phrases[i])
+        i+=1
